9c7157a2236c942ce1e59d597f5dd68443493ff2
alaricodev <alarico.pcsc@gmail.com>
2023-04-28 18:55:55 -0300

    first commit

---
+++ src/components/cardProduto.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="q-pa-md row items-start q-gutter-md" style="max-width=300px">
+    <q-card class="my-card">
+      <q-img :src="dados.foto" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="primary"
+          icon="add"
+          label="Detalhes"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%)"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">{{ dados.nome }}</div>
+          <div
+            class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"
+          ></div>
+        </div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">
+          R$ {{ dados.preco }} - {{ dados.peso }}
+        </div>
+        <div class="text-caption text-grey">
+          {{ dados.descricao }}
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon="shopping_cart" />
+        <q-btn flat color="primary"> Adicionar </q-btn>
+      </q-card-actions>
+    </q-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "cardProduto",
+  props: {
+    dados: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-page class="flex flex-center">
+    <!-- <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    > -->
+    <card-produto
+      v-for="(produto, index) in produtos"
+      :key="index"
+      :dados="produto"
+    />
+  </q-page>
+</template>
+
+<script>
+import cardProduto from "src/components/cardProduto.vue";
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  components: { cardProduto },
+  name: "IndexPage",
+  setup() {
+    return {
+      produtos: ref([
+        {
+          foto: "https://cdn.quasar.dev/img/chicken-salad.jpg",
+          nome: "Pão de Abóbora",
+          preco: "26,90",
+          peso: "900g",
+          descricao:
+            "Pão de abóbora sem glúten é um delicioso pão feito com farinha sem glúten e purê de abóbora. É macio, úmido e rico em sabor de abóbora, e é perfeito para quem busca uma opção sem glúten.",
+        },
+        {
+          foto: "https://cdn.quasar.dev/img/chicken-salad.jpg",
+          nome: "Pão de Ervas finas e tomate seco",
+          preco: "32,90",
+          peso: "900g",
+          descricao:
+            "O pão de ervas finas com tomate seco é um pão saboroso e aromático, que combina ervas e tomate seco. É macio por dentro e crocante por fora, e pode ser apreciado como lanche ou acompanhamento em refeições. Ideal para quem busca sabores mais complexos.",
+        },
+      ]),
+    };
+  },
+});
+</script>
